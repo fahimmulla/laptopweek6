d9407128b8d79d0b451d3ca708e05d1eaa170006
Fahim Mulla <fahim22@hotmail.co.uk>
2018-04-11 16:35:06 +0100

    new

--- script.js
+++ script.js
@@ -1,25 +1,25 @@
-function add(){
+function add() {
     var value1 = parseFloat(document.getElementById('n1').value);
     var value2 = parseFloat(document.getElementById('n2').value);
     var result = value1 + value2;
     alert(result);
 }
 
-function subract(){
+function subract() {
     var value1 = parseFloat(document.getElementById('n1').value);
     var value2 = parseFloat(document.getElementById('n2').value);
     var result = value1 - value2;
     alert(result);
 }
 
-function divide(){
+function divide() {
     var value1 = parseFloat(document.getElementById('n1').value);
     var value2 = parseFloat(document.getElementById('n2').value);
     var result = value1 / value2;
     alert(result);
 }
 
-function multiply(){
+function multiply() {
     var value1 = parseFloat(document.getElementById('n1').value);
     var value2 = parseFloat(document.getElementById('n2').value);
     var result = value1 * value2;
@@ -33,7 +33,7 @@ function sqaure() {
     alert(results)
 }
 
-function add(){
+function add() {
     var value1 = parseFloat(document.getElementById('n4').value);
     var value2 = parseFloat(document.getElementById('n5').value);
     var value3 = parseFloat(document.getElementById('n6').value);
@@ -41,7 +41,7 @@ function add(){
     alert(result);
 }
 
-function subract(){
+function subract() {
     var value1 = parseFloat(document.getElementById('n4').value);
     var value2 = parseFloat(document.getElementById('n5').value);
     var value3 = parseFloat(document.getElementById('n6').value);
@@ -49,7 +49,7 @@ function subract(){
     alert(result);
 }
 
-function divide(){
+function divide() {
     var value1 = parseFloat(document.getElementById('n4').value);
     var value2 = parseFloat(document.getElementById('n5').value);
     var value3 = parseFloat(document.getElementById('n6').value);
@@ -57,21 +57,34 @@ function divide(){
     alert(result);
 }
 
-function multiply(){
+function multiply() {
     var value1 = parseFloat(document.getElementById('n4').value);
     var value2 = parseFloat(document.getElementById('n5').value);
     var value3 = parseFloat(document.getElementById('n6').value);
-    var result = value1 * value2 *value3;
+    var result = value1 * value2 * value3;
     alert(result);
 }
 
 
-
-
-function person(){
-    var Name = parseFloat(document.getElementById('n7').value);
-    var occupation = parseFloat(document.getElementById('n8').value);
-    var Age = parseFloat(document.getElementById('n9').value);
-    var results =  "Name added to the system:" +person.name + "Age" +person.age + "occupation:" +person.occupation ;
-    alert(results);
+function maker(name, age, occupation) {
+    var it = {};
+    it.name = name;
+    it.age = age;
+    it.occupation = occupation;
+    return it;
 }
+
+function myfunction() {
+    var person = maker("fahim",21,"doctor");
+    alert(person.name + "      " + person.age + "          " + person.occupation + "           ");
+}
+
+function addperson() {
+    var name = document.getElementById("name");
+    var age = document.getElementById("age");
+    var occupation = document.getElementById("occupation");
+    var output = document.getElementById("outputTxt");
+    output.value = "Hi there," + name + "!" }
+
+
+
